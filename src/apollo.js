e73c3e4256a0508db3f04469a20bed2aeb1d40f3
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-29 23:45:12 +0900

    Apollo get query & add style

--- src/apollo.js
+++ src/apollo.js
@@ -2,6 +2,8 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/"
+  // 미리 만들어둔 api 사용
+  // https://github.com/jaeuk2274/movie-graphql.git
 });
 
 export default client;

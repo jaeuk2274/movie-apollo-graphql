31a33b93de7b90c77a544ed54b8b267187fe4224
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-30 20:01:55 +0900

    create isLiked button

--- src/apollo.js
+++ src/apollo.js
@@ -7,6 +7,17 @@ const client = new ApolloClient({
   resolvers: {
     Movie: {
       isLiked: () => false
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+            medium_cover_image: "lalalalal"
+          }
+        });
+      }
     }
   }
 });
